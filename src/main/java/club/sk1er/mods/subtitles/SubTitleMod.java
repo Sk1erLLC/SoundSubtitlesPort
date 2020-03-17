76a07c1adeb6559b1db59db95141a0ed4460d3b1
asbyth <cyronize@gmail.com>
2020-03-17 13:23:33 -0400

    update version

--- src/main/java/club/sk1er/mods/subtitles/SubTitleMod.java
+++ src/main/java/club/sk1er/mods/subtitles/SubTitleMod.java
@@ -18,7 +18,7 @@ import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType
 @Mod(modid = SubTitleMod.MODID, version = SubTitleMod.VERSION)
 public class SubTitleMod {
     public static final String MODID = "subtitles_mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static SubTitleMod instance;
     public GuiSubtitleOverlay guiSubtitleOverlay;
     private SubtitleConfig subtitleConfig;
